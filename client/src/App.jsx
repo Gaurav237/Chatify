9e7a683f2b817e8a6f396df6eaab1ad564059c6a
Gaurav237 <gktiwary237@gmail.com>
2024-05-24 22:39:52 +0530

    react-router-dom installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
+import { Button } from '@chakra-ui/react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ChatPage from './pages/ChatPage'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route index element={<HomePage /> } />
+        <Route path='/chat' element={<ChatPage /> } />
+      </Routes>
     </>
   )
 }
